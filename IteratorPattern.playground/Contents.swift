78a5c7ad6b93490049242a3d2d9f0baa2da5ceb9
ChungMai <macmini@MacMinis-Mac-mini.local>
2016-10-06 11:42:27 +0700

    Update for Swift 3

--- IteratorPattern.playground/Contents.swift
+++ IteratorPattern.playground/Contents.swift
@@ -2,38 +2,6 @@
 
 import UIKit
 
-//class Player{
-//    var name : String!
-//    
-//    init(name : String){
-//        self.name = name
-//    }
-//}
-//
-//struct ObjectCollection<T>{
-//    let items : [T]
-//}
-//
-//extension ObjectCollection : SequenceType{
-//    typealias Generator = AnyGenerator<T>
-//    func generate() -> Generator {
-//        var i = 0
-//        return AnyGenerator {(i >= self.items.count) ? nil : self.items[i++]}
-//    }
-//}
-//
-//let player1 = Player(name: "Helmi")
-//let player2 = Player(name: "Raphael")
-//let player3 = Player(name: "Adrien")
-//let player4 = Player(name: "Alain")
-//
-//let players = ObjectCollection(items:[player1,player2, player3,player4])
-//
-//for player in players{
-//    print("Name: \(player.name)")
-//}
-
-
 class Player{
     var name : String!
 
@@ -42,9 +10,32 @@ class Player{
     }
 }
 
-struct ObjectCollection<T>:Sequence, IteratorProtocol{
+
+// Swift 2
+
+//struct ObjectCollection<T>{
+//    let items : [T]
+//}
+//extension ObjectCollection : SequenceType{
+//    typealias Generator = AnyGenerator<T>
+//    func generate() -> Generator {
+//        var i = 0
+//        return AnyGenerator {(i >= self.items.count) ? nil : self.items[i++]}
+//    }
+//}
+
+
+// Swift 3
+struct ObjectCollection<T>{
     let items : [T]
     var count: Int = 0
+    init(items : [T]){
+        self.items = items
+    }
+}
+
+extension ObjectCollection:Sequence, IteratorProtocol{
+    
     mutating func next() -> T? {
         
         if count >= self.items.count {
@@ -56,13 +47,12 @@ struct ObjectCollection<T>:Sequence, IteratorProtocol{
     }
 }
 
-
 let player1 = Player(name: "Helmi")
 let player2 = Player(name: "Raphael")
 let player3 = Player(name: "Adrien")
 let player4 = Player(name: "Alain")
 
-let players = ObjectCollection(items:[player1,player2, player3,player4], count : 0)
+let players = ObjectCollection(items:[player1,player2, player3,player4])
 
 for player in players{
     print("Name: \(player.name)")
